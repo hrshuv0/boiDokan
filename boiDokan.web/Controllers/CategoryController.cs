8ca8f6ba1eb41ff999c1ce033454aa9a1980bcab
hrshuv0 <h.r.shuvo9@gmail.com>
2022-06-19 22:29:58 +0600

    added edit and delete functionality

--- boiDokan.web/Controllers/CategoryController.cs
+++ boiDokan.web/Controllers/CategoryController.cs
@@ -21,13 +21,12 @@ public class CategoryController : Controller
         return View(objCategoryList);
     }
     
-    // GET
+    
     public IActionResult Create()
     {
         return View();
     }
     
-    // POST
     [HttpPost]
     [ValidateAntiForgeryToken]
     public IActionResult Create(Category model)
@@ -43,4 +42,63 @@ public class CategoryController : Controller
         
         return RedirectToAction(nameof(Index));
     }
+    
+    [HttpGet]
+    public IActionResult Edit(int? id)
+    {
+        if (id is null or 0) return NotFound();
+
+        var category = _dbContext.Categories!.Find(id);
+        // var categoryFirst = _dbContext.Categories!.FirstOrDefault(c => c.Id == id);
+        if (category is null) return NotFound();
+        
+        return View(category);
+    }
+    
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category model)
+    {
+        if (model.Name == model.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+        }
+        if (!ModelState.IsValid) return View(model);
+
+        _dbContext.Categories!.Update(model);
+        _dbContext.SaveChanges();
+        
+        return RedirectToAction(nameof(Index));
+    }
+    
+    
+    [HttpGet]
+    public IActionResult Delete(int? id)
+    {
+        if (id is null or 0) return NotFound();
+
+        var category = _dbContext.Categories!.Find(id);
+        // var categoryFirst = _dbContext.Categories!.FirstOrDefault(c => c.Id == id);
+        if (category is null) return NotFound();
+        
+        return View(category);
+    }
+    
+    
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePost(int? id)
+    {
+        var category = _dbContext.Categories!.Find(id);
+
+        if (category is null) return NotFound();
+
+        _dbContext.Categories.Remove(category);
+        _dbContext.SaveChanges();
+        
+        return RedirectToAction(nameof(Index));
+    }
+    
+    
 }
