94d25450610733da3dce1b3f1d138a4ba130522a
hrshuv0 <h.r.shuvo9@gmail.com>
2022-07-17 15:43:59 +0600

    added register as company

--- boiDokan.entities/Models/ApplicationUser.cs
+++ boiDokan.entities/Models/ApplicationUser.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace boiDokan.entities.Models;
 
@@ -11,4 +13,9 @@ public class ApplicationUser : IdentityUser
     public string? City { get; set; }
     public string? State { get; set; }
     public string? PostalCode { get; set; }
+
+    public int? CompanyId { get; set; }
+    [ForeignKey("CompanyId")]
+    [ValidateNever]
+    public Company? Company { get; set; }
 }
--- boiDokan.web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ boiDokan.web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using boiDokan.dal.Repository.IRepository;
 using boiDokan.entities.Models;
 using boiDokan.utility;
 using Microsoft.AspNetCore.Authentication;
@@ -34,6 +35,7 @@ namespace boiDokan.web.Areas.Identity.Pages.Account
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUnitOfWork _unitOfWork;
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
@@ -41,7 +43,8 @@ namespace boiDokan.web.Areas.Identity.Pages.Account
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
             IEmailSender emailSender, 
-            RoleManager<IdentityRole> roleManager)
+            RoleManager<IdentityRole> roleManager, 
+            IUnitOfWork unitOfWork)
         {
             _userManager = userManager;
             _userStore = userStore;
@@ -50,6 +53,7 @@ namespace boiDokan.web.Areas.Identity.Pages.Account
             _logger = logger;
             _emailSender = emailSender;
             _roleManager = roleManager;
+            _unitOfWork = unitOfWork;
         }
 
         /// <summary>
@@ -114,8 +118,14 @@ namespace boiDokan.web.Areas.Identity.Pages.Account
             public string? PhoneNumber { get; set; }
             public string? Role { get; set; }
 
+            public int? CompanyId { get; set; }
+            
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+            
+            [ValidateNever]
+            public IEnumerable<SelectListItem> CompanyList { get; set; }
+
         }
 
 
@@ -138,6 +148,11 @@ namespace boiDokan.web.Areas.Identity.Pages.Account
                 {
                     Text = i,
                     Value = i
+                }),
+                CompanyList = _unitOfWork.Company.GetAll().Select(i => new SelectListItem()
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
                 })
             };
         }
@@ -159,6 +174,11 @@ namespace boiDokan.web.Areas.Identity.Pages.Account
                 user.PostalCode = Input.PostalCode;
                 user.Name = Input.Name;
                 user.PhoneNumber = Input.PhoneNumber;
+
+                if (Input.Role == SD.RoleUserCompany)
+                {
+                    user.CompanyId = Input.CompanyId;
+                }
                 
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
