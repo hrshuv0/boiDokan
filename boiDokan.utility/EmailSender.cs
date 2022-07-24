c0f143a998298419c14d2578b0010e2a091f604f
hrshuv0 <h.r.shuvo9@gmail.com>
2022-07-24 15:38:13 +0600

    added email sender configuration - mailjet

--- boiDokan.utility/EmailSender.cs
+++ boiDokan.utility/EmailSender.cs
@@ -1,26 +1,85 @@
+using Mailjet.Client;
+using Mailjet.Client.Resources;
 using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
 using MimeKit;
+using Newtonsoft.Json.Linq;
 
 namespace boiDokan.utility;
 
 public class EmailSender : IEmailSender
 {
-    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    private readonly IConfiguration _configuration;
+    public MailJetSettings _mailJetOptions;
+    
+    public EmailSender(IConfiguration configuration)
     {
-        var emailToSend = new MimeMessage();
-        emailToSend.From.Add(MailboxAddress.Parse("username@email.com"));
-        emailToSend.To.Add(MailboxAddress.Parse(email));
-        emailToSend.Subject = subject;
-        emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){Text = htmlMessage};
-
-        using (var emailClient = new SmtpClient())
-        {
-            emailClient.Connect("smtppro.zoho.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-            emailClient.Authenticate("username", "pass");
-            emailClient.Send(emailToSend);
-            emailClient.Disconnect(true);
-        }
-        return Task.CompletedTask;
+        _configuration = configuration;
+        _mailJetOptions = _configuration.GetSection("MailJet").Get<MailJetSettings>();
+    }
+    public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        // var emailToSend = new MimeMessage();
+        // emailToSend.From.Add(MailboxAddress.Parse("email.com"));
+        // emailToSend.To.Add(MailboxAddress.Parse(email));
+        // emailToSend.Subject = subject;
+        // emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){Text = htmlMessage};
+        //
+        // using (var emailClient = new SmtpClient())
+        // {
+        //     emailClient.Connect("smtppro.zoho.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+        //     emailClient.Authenticate("username", "password");
+        //     emailClient.Send(emailToSend);
+        //     emailClient.Disconnect(true);
+        // }
+        // return Task.CompletedTask;
+        
+        MailjetClient client = new MailjetClient(_mailJetOptions.ApiKey, _mailJetOptions.SecretKey) {
+            Version = ApiVersion.V3_1,
+        };
+        MailjetRequest request = new MailjetRequest {
+                Resource = Send.Resource,
+            }
+            .Property(Send.Messages, new JArray {
+                new JObject {
+                    {
+                        "From",
+                        new JObject {
+                            {"Email", "info@boidokan.live"}, 
+                            {"Name", "boiDokan"}
+                        }
+                    }, {
+                        "To",
+                        new JArray {
+                            new JObject {
+                                {
+                                    "Email",
+                                    email
+                                }, {
+                                    "Name",
+                                    "boiDokan"
+                                }
+                            }
+                        }
+                    }, {
+                        "Subject",
+                        subject
+                    }, {
+                        "HTMLPart",
+                        htmlMessage
+                    }
+                }
+            });
+        await client.PostAsync(request);
+        // if (response.IsSuccessStatusCode) {
+        //     Console.WriteLine(string.Format("Total: {0}, Count: {1}\n", response.GetTotal(), response.GetCount()));
+        //     Console.WriteLine(response.GetData());
+        // } else {
+        //     Console.WriteLine(string.Format("StatusCode: {0}\n", response.StatusCode));
+        //     Console.WriteLine(string.Format("ErrorInfo: {0}\n", response.GetErrorInfo()));
+        //     Console.WriteLine(response.GetData());
+        //     Console.WriteLine(string.Format("ErrorMessage: {0}\n", response.GetErrorMessage()));
+        // }
     }
 }
---
+++ boiDokan.utility/MailJetSettings.cs
@@ -0,0 +1,7 @@
+namespace boiDokan.utility;
+
+public class MailJetSettings
+{
+    public string ApiKey { get; set; }
+    public string SecretKey { get; set; }
+}
